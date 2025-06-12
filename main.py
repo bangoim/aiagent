8c8722a0f4bc19d3e5173aefb32c0aac809ca40c
bangoim <joaocbbc@outlook.com>
2025-06-11 23:18:34 -0300

    feat: chat now stores message history

--- main.py
+++ main.py
@@ -21,15 +21,17 @@ def main():
     client = genai.Client(api_key=api_key)
 
     messages = [
-    types.Content(role="user", parts=[types.Part(text=messages)]),
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents=user_prompt)
+    generate_content(client, messages)
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
     print("Response:")
     print(response.text)
 
