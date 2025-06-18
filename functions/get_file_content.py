f1629a67fac0b2a745b7787ce569f75fa9bad57b
bangoim <joaocbbc@outlook.com>
2025-06-18 09:30:05 -0300

    feat: create call_function.py for LLM usage of functions and update functions schema

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,6 @@
 import os
+from google.genai import types
+from config import MAX_CHARS
 
 
 def get_file_content(working_directory, file_path):
@@ -10,8 +12,6 @@ def get_file_content(working_directory, file_path):
     if not os.path.isfile(target_file_path):
         f'Error: File not found or is not a regular file: "{file_path}"'
 
-    MAX_CHARS = 10_000
-
     try:
         with open(target_file_path, "r") as f:
             file_content_string = f.read(MAX_CHARS)
@@ -22,3 +22,18 @@ def get_file_content(working_directory, file_path):
         return f'{file_content_string}\n[...File "{target_file_path}" truncated at 10000 characters]'
     except Exception as e:
         return f"Error: {e}"
+
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Reads and returns the first {MAX_CHARS} characters of the content from a specified file within the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file whose content should be read, relative to the working directory.",
+            ),
+        },
+    ),
+)
